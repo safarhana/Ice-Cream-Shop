404947cac347a1c5f6eda38cbd231e008f9e3113
Siam4201 <siamsarker36@gmail.com>
2026-01-01 02:30:37 +0600

    user_style_searchbar_bug

--- js/User_script.js
+++ js/User_script.js
@@ -1,1 +1,18 @@
 let profile=document.querySelector('.header .flex .profile-detail');
+document.querySelector('#user-btn').onclick=()=>{
+    profile.classList.toggle('active');
+    searchForm.classList.remove('active');
+
+};
+
+let searchForm = document.querySelector('.header .flex .search-form');
+document.querySelector('#search-btn').onclick=()=>{
+    searchForm.classList.toggle('active');
+    profile.classList.remove('active');
+};
+
+let navbar=document.querySelector('navbar');
+document.querySelector('#menu-btn').onclick=()=>{
+    navbar.classList.toggle('active');
+};
+

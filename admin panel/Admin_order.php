0e43f55e6e1abd95da567f2447759e103b600fa1
safarhana <farhana.mim080@gmail.com>
2026-01-01 03:02:26 +0600

    half of admin prder done

---
+++ admin panel/Admin_order.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ice Cream Delights - Orders Page</title>
+    <link rel="stylesheet" type="text/css" href="../css/Admin_style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+    <!--Boxicons  CDN link -->
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css">
+</head>
+<body>
+
+        <div class ="main-container">
+          <?php include '../components/Admin_header.php'; ?>
+
+          <section class="order-container">
+          <div class ="heading">
+              <h1>Total order placed </h1>
+              <img src="../image/separator-img.png">
+           </div>
+
+            
+
+
+
+
+        </section>
+    </div>
+
+
+    <link rel="stylesheet" type="text/css" href="../css/Admin_style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+    <!--Boxicons  CDN link -->
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css">
+
+</body>
+</html>
